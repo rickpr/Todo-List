5646f16fb5336752100216d518df8a349eb102b9
Ricardo Piro-Rael <fdisk87@yahoo.com>
2022-08-01 02:49:43 -0700

    Initial Commit

---
+++ Shared/Todo_ListApp.swift
@@ -0,0 +1,20 @@
+//
+//  Todo_ListApp.swift
+//  Shared
+//
+//  Created by fdisk on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
